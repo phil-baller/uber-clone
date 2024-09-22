afc388904ab0036cf2d49b2193dd57e3c5973245
Philemon Tebo Tebo <philballer41@gmail.com>
2024-09-22 01:11:24 +0100

    installing prettier and eslint for better code formatting

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
-import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className='font-medium text-2xl text-gray-300'>Uber clone</Text>
     </SafeAreaView>
   );
 }
